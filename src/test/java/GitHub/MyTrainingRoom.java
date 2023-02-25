9edce69585a70783c82c966cbd7aef4af5c3e5b9
Stanislav K <fallenqa@yandex.ru>
2023-02-25 11:19:10 +0300

    add new test on GitHub search selenide

---
+++ src/test/java/GitHub/MyTrainingRoom.java
@@ -0,0 +1,31 @@
+package GitHub;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class MyTrainingRoom {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com/";
+    }
+
+    @Test
+    void myFirstLifeCode() {
+        open("https://github.com/");
+        $("[placeholder='Search GitHub']").setValue("selenide").pressEnter();
+        $$("ul.repo-list li").first().$("a").click();
+        $("#repository-container-header").shouldHave(text("selenide / selenide"));
+        sleep(5000);
+
+
+
+    }
+}
